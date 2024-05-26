8af7b4668ce4d408146b8efe0dc76ecb7705669e
park <parkhe@cua.edu>
2024-05-26 10:35:45 -0400

    Create twosheets in Excel

--- CreateExcel.ps1
+++ CreateExcel.ps1
@@ -3,18 +3,26 @@
 Import-Module ImportExcel
 
 # Define the path for the Excel file
-$excelFilePath = "C:\Users\jiaoxue\Documents\excel2csv\example.xlsx"
+$excelFilePath = "C:\Users\jiaoxue\Documents\0526_24\Excel_twosheets.xlsx"
 
 # Create sample data
-$data = @(
+$data_1 = @(
+    [PSCustomObject]@{ Name = "John Doe"; Age = 30; City = "New York" }
+    [PSCustomObject]@{ Name = "Jane Smith"; Age = 25; City = "Los Angeles" }
+    [PSCustomObject]@{ Name = "Sam Brown"; Age = 35; City = "Chicago" }
+)
+
+$data_2 = @(
     [PSCustomObject]@{ Name = "John Doe"; Age = 30; City = "New York" }
     [PSCustomObject]@{ Name = "Jane Smith"; Age = 25; City = "Los Angeles" }
     [PSCustomObject]@{ Name = "Sam Brown"; Age = 35; City = "Chicago" }
 )
 
 # Export data to Excel
-$data | Export-Excel -Path $excelFilePath -WorksheetName "Sheet1"
+$data_1 | Export-Excel -Path $excelFilePath -WorksheetName "Sheet1"
+$data_2 | Export-Excel -Path $excelFilePath -WorksheetName "Sheet2"
 
 Write-Output "Example Excel file has been created: $excelFilePath"
 
 Pause
+
